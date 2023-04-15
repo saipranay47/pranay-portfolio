f6a3fd5ff7a830d3119fd48487e26ce7e474f9cb
saipranay47 <saipranayadepu07@gmail.com>
2023-04-15 13:04:34 +0530

    removed unwanted images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,6 @@ import Link from "next/link";
 
 
 import { Card } from "@/components/Card";
-import { SimpleLayout } from "@/components/SimpleLayout";
-import logoAnimaginary from "@/images/logos/animaginary.svg";
-import logoCosmos from "@/images/logos/cosmos.svg";
-import logoHelioStream from "@/images/logos/helio-stream.svg";
-import logoOpenShuttle from "@/images/logos/open-shuttle.svg";
-import logoPlanetaria from "@/images/logos/planetaria.svg";
 import amazoncloneBanner from "@/images/banners/amazonclone.png";
 import ticketBanner from "@/images/banners/ticket.png";
 import vtappBanner from "@/images/banners/vtapp.png";
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,11 @@ import {
   LinkedInIcon,
   BehanceIcon,
 } from "@/components/SocialIcons";
-import image1 from "@/images/photos/image-1.jpg";
-import image2 from "@/images/photos/image-2.jpg";
-import image3 from "@/images/photos/image-3.jpg";
-import image4 from "@/images/photos/image-4.jpg";
-import image5 from "@/images/photos/image-5.jpg";
-import logoDevProtocol from "@/images/logos/devprotocol.png";
+// import image1 from "@/images/photos/image-1.jpg";
+// import image2 from "@/images/photos/image-2.jpg";
+// import image3 from "@/images/photos/image-3.jpg";
+// import image4 from "@/images/photos/image-4.jpg";
+// import image5 from "@/images/photos/image-5.jpg";
 import tinythings from "@/images/logos/tinythings.jpeg";
 import vtapp from "@/images/logos/vtapp.jpeg";
 import osc from "@/images/logos/osc.jpeg";
@@ -94,34 +93,34 @@ function SocialLink({ icon: Icon, ...props }) {
   );
 }
 
-function Newsletter() {
-  return (
-    <form
-      action="/thank-you"
-      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-    >
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
-      </h2>
-      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
-      </p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-blue-400 dark:focus:ring-blue-400/10 sm:text-sm"
-        />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
-    </form>
-  );
-}
+// function Newsletter() {
+//   return (
+//     <form
+//       action="/thank-you"
+//       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+//     >
+//       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+//         <MailIcon className="h-6 w-6 flex-none" />
+//         <span className="ml-3">Stay up to date</span>
+//       </h2>
+//       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+//         Get notified when I publish something new, and unsubscribe at any time.
+//       </p>
+//       <div className="mt-6 flex">
+//         <input
+//           type="email"
+//           placeholder="Email address"
+//           aria-label="Email address"
+//           required
+//           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-blue-400 dark:focus:ring-blue-400/10 sm:text-sm"
+//         />
+//         <Button type="submit" className="ml-4 flex-none">
+//           Join
+//         </Button>
+//       </div>
+//     </form>
+//   );
+// }
 
 function Resume() {
   let resume = [
@@ -210,38 +209,38 @@ function Resume() {
   );
 }
 
-function Photos() {
-  let rotations = [
-    "rotate-2",
-    "-rotate-2",
-    "rotate-2",
-    "rotate-2",
-    "-rotate-2",
-  ];
+// function Photos() {
+//   let rotations = [
+//     "rotate-2",
+//     "-rotate-2",
+//     "rotate-2",
+//     "rotate-2",
+//     "-rotate-2",
+//   ];
 
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              "relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl",
-              rotations[imageIndex % rotations.length]
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="mt-16 sm:mt-20">
+//       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+//         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+//           <div
+//             key={image.src}
+//             className={clsx(
+//               "relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl",
+//               rotations[imageIndex % rotations.length]
+//             )}
+//           >
+//             <Image
+//               src={image}
+//               alt=""
+//               sizes="(min-width: 640px) 18rem, 11rem"
+//               className="absolute inset-0 h-full w-full object-cover"
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
 export default function Home({ articles }) {
   return (
